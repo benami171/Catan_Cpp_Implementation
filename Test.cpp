08f89b563f627f5745835d183410a416658a1b24
Gal Ben Ami <benami171@gmail.com>
2024-06-16 12:43:55 +0300

    divides tests into subcases.

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include "Player.hpp"
 #include "CatanGame.hpp"
-
+#include "Player.hpp"
+#include "doctest.h"
 
 using namespace std;
 using namespace Catan;
@@ -12,7 +11,15 @@ Player _p2("P2");
 Player _p3("P3");
 CatanGame game(_p1, _p2, _p3);
 
-TEST_CASE("Playing out of turn"){
+void before_each() {
+    _p1 = Player("P1");
+    _p2 = Player("P2");
+    _p3 = Player("P3");
+    game = CatanGame(_p1, _p2, _p3);
+}
+
+TEST_CASE("Playing out of turn") {
+    before_each();
     game.startTurn("P1");
     CHECK_THROWS(_p2.placeInitialSettlement(13, game));
     CHECK_THROWS(_p3.placeSettlement(12, game));
@@ -23,14 +30,28 @@ TEST_CASE("Playing out of turn"){
     CHECK_THROWS(_p3.rollDice(6));
 }
 
-TEST_CASE("Invalid INITIAL settlement/road placement"){
+TEST_CASE("Invalid INITIAL settlement/road placement") {
+    before_each();
     game.startTurn("P1");
     _p1.placeInitialSettlement(2, game);
-    CHECK_THROWS(_p1.placeInitialRoad(8, game)); // too far from settlement
     _p1.placeInitialRoad(5, game);
     game.endTurn();
-    CHECK_THROWS(_p2.placeInitialSettlement(5, game)); // too close to P1's settlement
-    CHECK_THROWS(_p2.placeInitialSettlement(54, game)); // OUT OF BOUNDS
     _p2.placeInitialSettlement(10, game);
+    _p2.placeInitialRoad(17, game);
+    _p2.placeInitialSettlement(13, game);
+    _p2.placeInitialRoad(13, game);
 
+    SUBCASE("placing road too far from settlement") {
+        CHECK_THROWS(_p1.placeInitialRoad(8, game));  // too far from settlement
+    }
+
+    SUBCASE("placing road too close to settlement") {
+        CHECK_THROWS(_p2.placeInitialSettlement(5, game));  // too close to P1's settlement
+    }
+
+    SUBCASE("placing 3rd initial settlement") {
+        CHECK_THROWS(_p2.placeInitialSettlement(22, game));  // already placed 2 initial settlements.
+    }
+
+    CHECK_THROWS(_p2.placeInitialSettlement(54, game));  // OUT OF BOUNDS
 }
